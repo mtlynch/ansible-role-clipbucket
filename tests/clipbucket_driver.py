768cbecda8313a4e03d81a02f859ea4e18238f08
Michael Lynch <mtlynch@google.com>
2016-09-19 22:55:54 -0400

    Tweaking selenium tests so that logs are visible in Travis (#31)
    * Tweaking selenium tests so that logs are visible in Travis
    
    * Getting rid of unnecessary ansible.cfg command
    
    * Reorganizing build order

--- tests/clipbucket_driver.py
+++ tests/clipbucket_driver.py
@@ -4,6 +4,8 @@ ClipBucketDriver drives a web UI for ClipBucket, using the Selenium WebDriver
 API.
 """
 
+import logging
+
 from selenium import webdriver
 from selenium.common import exceptions
 from selenium.webdriver.common.keys import Keys
@@ -12,6 +14,8 @@ from selenium.webdriver.support import ui
 
 # Standard timeout (in seconds) to wait for a page element to appear.
 TIMEOUT = 5
+
+logger = logging.getLogger(__name__)
 
 
 class Error(Exception):
@@ -29,9 +33,12 @@ class ClipBucketDriver(object):
         self._base_url = base_url
 
     def get(self, relative_url):
-        self._driver.get(self._base_url + relative_url)
+        full_url = self._base_url + relative_url
+        logger.info('Loading url: %s', full_url)
+        self._driver.get(full_url)
 
     def do_login(self, username, password):
+        logger.info('Logging in with username=%s', username)
         self.get('/admin_area/login.php')
 
         username_element = self._driver.find_element_by_id('username')
@@ -43,8 +50,10 @@ class ClipBucketDriver(object):
         username_element.send_keys(username)
         password_element.send_keys(password)
         self._driver.find_element_by_name('login').click()
+        logger.info('Login complete')
 
     def do_check_modules(self):
+        logger.info('Checking ClipBucket modules')
         self.get('/admin_area/cb_mod_check.php')
 
         for module_element in self._driver.find_elements_by_class_name('well'):
@@ -56,7 +65,9 @@ class ClipBucketDriver(object):
             except exceptions.NoSuchElementException:
                 # Lack of alert is good: the module is installed correctly.
                 continue
+        logger.info('Module check complete')
 
     def quit(self):
+        logger.info('Exiting ClipBucket driver')
         self._driver.quit()
 
--- tests/drive_web_flow_main.py
+++ tests/drive_web_flow_main.py
@@ -1,12 +1,25 @@
 import argparse
+import logging
 
 import clipbucket_driver
 
-BASE_URL = 'http://localhost'
+logger = logging.getLogger(__name__)
+
+
+def _configure_logging():
+    """Configure the root logger for log output."""
+    root_logger = logging.getLogger()
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+    handler.setFormatter(formatter)
+    root_logger.addHandler(handler)
+    root_logger.setLevel(logging.INFO)
 
 
 def main(args):
-    driver = clipbucket_driver.ClipBucketDriver(BASE_URL)
+    _configure_logging()
+    driver = clipbucket_driver.ClipBucketDriver(args.url)
     driver.do_login(args.username, args.password)
     driver.do_check_modules()
 
@@ -17,6 +30,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         prog='ClipBucket Web Driver',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--url', required=True)
     parser.add_argument('--username', required=True)
     parser.add_argument('--password', required=True)
     main(parser.parse_args())
